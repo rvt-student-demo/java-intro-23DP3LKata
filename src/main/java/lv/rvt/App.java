3b2ea69d922a465c95651b2bbbf8d72d02b1a611
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-09-06 11:20:50 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,31 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+        
+        // String input1 = scanner.nextLine();
+        // System.out.println("Sveiks " + input1 + "!");
 
+        // String name = "John";
+        // int age = 20;
+        // boolean isOnline = true;
+        // System.out.println(name);
+
+        System.out.println("Ievadi savu vārdu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvārdu: ");
+        String lastname = scanner.nextLine();
+        
+        System.out.println("Ievadi savu grupu: ");
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + name + " " + lastname + ", grupa: " + group);
     }
 
 }
