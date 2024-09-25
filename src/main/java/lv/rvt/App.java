0a0799ee2688445d1380a5c86971550f67354448
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-09-25 07:18:05 +0000

    1 part: Conditional statements and conditional operation / Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,76 +8,21 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        // 1 stunda
-        
-        // String input1 = scanner.nextLine();
-        // System.out.println("Sveiks " + input1 + "!");
-
-        // String name = "John";
-        // int age = 20;
-        // boolean isOnline = true;
-        // System.out.println(name);
-
-
-        // 2 stunda
-
-        // System.out.println("Ievadi savu vārdu: ");
-        // String name = scanner.nextLine();
-
-        // System.out.println("Ievadi savu uzvārdu: ");
-        // String lastname = scanner.nextLine();
-        
-        // System.out.println("Ievadi savu grupu: ");
-        // String group = scanner.nextLine();
-
-        // System.out.println("Students: " + name + " " + lastname + ", grupa: " + group);
-
-
-        // 3 stunda
-
-        // System.out.println("Print a value: ");
-        // int value = Integer.valueOf(scanner.nextLine());
-        // System.out.println("You wrote " + value);
-
-
-
-        // 1 part: Conditional statements and conditional operation 
-        // Speeding Ticket
-
-        // System.out.println("Give speed:");
-        // int input1 = scanner.nextInt();   // int input1 = Integer.valueOf(scanner.nextLine());
-        // if (input1 > 120) {
-        //     System.out.println("Speeding ticket!");
-        // }
-
-
-        // Grades and Points
-
-        System.out.println("Give points [0-100]:");
-        int input = scanner.nextInt();
-        if (input < 0) {
-            System.out.println("Grade: impossible!");
-        }
-        else if (0 < input && input <= 49) {
-            System.out.println("Grade: failed");
-        }
-        else if (50 <= input && input <= 59) {
-            System.out.println("Grade: 1");
-        }
-        else if (60 <= input && input <= 69) {
-            System.out.println("Grade: 2");
-        }
-        else if (70 <= input && input <= 79) {
-            System.out.println("Grade: 3");
-        }
-        else if (80 <= input && input <= 89) {
-            System.out.println("Grade: 4");
-        }
-        else if (90 <= input && input <= 100) {
-            System.out.println("Grade: 5");
-        }
-        else if (input > 100) {
-            System.out.println("Grade: incredible!");
+        // Gift tax
+        System.out.println("Value of the gift?");
+        int gift = scanner.nextInt();
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift >= 5000 && gift <= 25000) {
+            System.out.println("Tax: " + (100 + (gift-5000) * 0.08));
+        } else if (gift >= 25000 && gift <= 55000) {
+            System.out.println("Tax: " + (1700 + (gift-25000) * 0.1));
+        } else if (gift >= 55000 && gift <= 200000) {
+            System.out.println("Tax: " + (4700 + (gift-55000) * 0.12));
+        } else if (gift >= 200000 && gift <= 1000000) {
+            System.out.println("Tax: " + (22100 + (gift-200000) * 0.15));
+        } else if (gift >= 1000000) {
+            System.out.println("Tax: " + (142100 + (gift-1000000) * 0.17));
         }
     }
 
