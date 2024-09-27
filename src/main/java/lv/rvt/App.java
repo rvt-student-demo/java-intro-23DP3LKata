bbf10745d290535246e1f313782217d50ac12dc9
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-09-27 06:15:00 +0000

    2 part: Repeating functionality / Carry on?

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,22 +8,13 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        // Gift tax
-        System.out.println("Value of the gift?");
-        int gift = scanner.nextInt();
-        if (gift < 5000) {
-            System.out.println("No tax!");
-        } else if (gift >= 5000 && gift <= 25000) {
-            System.out.println("Tax: " + (100 + (gift-5000) * 0.08));
-        } else if (gift >= 25000 && gift <= 55000) {
-            System.out.println("Tax: " + (1700 + (gift-25000) * 0.1));
-        } else if (gift >= 55000 && gift <= 200000) {
-            System.out.println("Tax: " + (4700 + (gift-55000) * 0.12));
-        } else if (gift >= 200000 && gift <= 1000000) {
-            System.out.println("Tax: " + (22100 + (gift-200000) * 0.15));
-        } else if (gift >= 1000000) {
-            System.out.println("Tax: " + (142100 + (gift-1000000) * 0.17));
+        // Carry on?
+        while (true) {
+            System.out.println("Shall we carry on?");
+            String input = scanner.nextLine();
+            if (input.equals("no")) {
+                break;
+            }
         }
     }
-
 }
