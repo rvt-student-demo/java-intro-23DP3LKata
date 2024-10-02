4c82696d3f7748f28906cec59815f1f7df4d7efc
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-10-02 07:08:26 +0000

    2 part: More loops / Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,16 +8,30 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        // while
-        // int x = 10;
-        // while (x < 100) {
-        //     System.out.println(x);
-        //     x += 10;
-        // }
+        // Repeating, breaking and remembering (5 parts)
+        System.out.println("Give numbers:");
 
-        // for
-        for (int x = 10; x < 100; x += 10) {
-            System.out.println(x);
+        // int number;
+        int sum = 0; int numbers = 0; int even = 0; int odd = 0;
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + sum);
+                System.out.println("Numbers: " + numbers);
+                System.out.println("Average: " + Double.valueOf(sum)/numbers);
+                System.out.println("Even: " + even);
+                System.out.println("Odd: " + odd);
+                break;
+            }
+            sum += number; numbers++;
+            if (number % 2 == 0) {
+                even++;
+            } else if (number % 2 != 0) {
+                odd++;
+            }
+        
         }
     }
 }
