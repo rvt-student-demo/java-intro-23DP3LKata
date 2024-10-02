319a5958539472a4bb36024c2bc60fd6ab0e9148
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-10-02 07:38:55 +0000

    2 part: Methods and dividing the program into smaller parts / Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,30 +8,16 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        // Repeating, breaking and remembering (5 parts)
-        System.out.println("Give numbers:");
-
-        // int number;
-        int sum = 0; int numbers = 0; int even = 0; int odd = 0;
-
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == -1) {
-                System.out.println("Thx! Bye!");
-                System.out.println("Sum: " + sum);
-                System.out.println("Numbers: " + numbers);
-                System.out.println("Average: " + Double.valueOf(sum)/numbers);
-                System.out.println("Even: " + even);
-                System.out.println("Odd: " + odd);
-                break;
-            }
-            sum += number; numbers++;
-            if (number % 2 == 0) {
-                even++;
-            } else if (number % 2 != 0) {
-                odd++;
-            }
-        
+        int i = 1;
+        System.out.println("How many times?");
+        int number = Integer.valueOf(scanner.nextLine());
+        while (i <= number){
+            printText();
+            i += 1;
         }
+    }
+    public static void printText() 
+    {
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
