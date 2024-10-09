86f5821c056162ccfa3d24e18dafdd8f4c34771f
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-10-09 06:12:36 +0000

    2 part: Methods and dividing the program into smaller parts / From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,17 +7,15 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-
-        int i = 1;
-        System.out.println("How many times?");
-        int number = Integer.valueOf(scanner.nextLine());
-        while (i <= number){
-            printText();
-            i += 1;
-        }
+        // From one to parameter
+        printUntilNumber(5);
+        System.out.println("");
+        printUntilNumber(2);
     }
-    public static void printText() 
-    {
-        System.out.println("In a hole in the ground there lived a method");
+    public static void printUntilNumber(int number){
+        int i = 1;
+        while(i <= number){
+            System.out.println(i++);
+        }
     }
 }
