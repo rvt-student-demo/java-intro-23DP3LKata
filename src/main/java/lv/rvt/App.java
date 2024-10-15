d02883655583f04bc54437b24c5b20982e73ff31
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-10-15 18:34:04 +0000

    2 part: Methods and dividing the program into smaller parts / Star sign (4 parts) 2 version

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,21 +8,15 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        // Star sign (4 parts)
-        System.out.println("How many columns?");
-        int columns = Integer.valueOf(scanner.nextInt());
-        int[] starsInRow = new int[columns];
-
-        for (int i = 1; i <= columns; i++) {
-            System.out.println("How many stars are in the " + i + " row?");
-            starsInRow[i - 1] = Integer.valueOf(scanner.nextInt());
-        }
+        // Advanced astrology (3 parts)
+        Stars.printStars(5);
+        Stars.printStars(3);
+        Stars.printStars(9);
         System.out.println();
-        for (int stars : starsInRow) {
-            for (int j = 0; j < stars; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        Stars.printSquare(4);
+        System.out.println();
+        Stars.printRectangle(17,3);
+        System.out.println();
+        Stars.printTriangle(4);
     }
 }
