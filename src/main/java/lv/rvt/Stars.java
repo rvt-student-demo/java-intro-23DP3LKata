6497ccbdee1f88557727d1a13b8491921545917d
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-10-15 18:33:31 +0000

    Stars.java file update

--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -2,7 +2,37 @@ package lv.rvt;
 import java.util.*;
 
 public class Stars{
-    public static void printStars() {
-        System.out.println("*");
+    Scanner scanner = new Scanner(System.in);
+
+    public static void printStars(int stars){
+        for (int i = 0; i < stars; i++){
+            System.out.print("*");
+        
+        }
+        System.out.println("");
+    }
+    
+    public static void printSquare(int size){
+        printStars(4);
+        printStars(4);
+        printStars(4);
+        printStars(4);
+    }
+    public static void printRectangle(int width, int height){
+        for (int i = 0; i < height; i++){
+            System.out.println("");
+            for (int j = 0; j < width; j++){
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+    }
+    public static void printTriangle(int size){
+        for (int i = 0; i <= size; i++){
+            for(int j = 0; j < i; j++)
+            System.out.print("*");
+            System.out.println();
+        }
+        
     }
 }
