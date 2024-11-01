4c3945552f88d1d2717bb099a3858a8bdf4b10ea
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-11-01 07:11:54 +0000

    3 part: Using strings / Personal details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,20 +8,31 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        // AV Club
+        // Personal details
+
+
+        String lname = ""; int count = 0; int sumYear = 0;
 
         while(true){
             String text = scanner.nextLine();
             if (text.equals("")){
                 break;
-            } 
-            
-            String[] split = text.split(" ");
-            for (String word : split){
-                if (word.contains("av")){
-                    System.out.println(word);
-                }
             }
+
+            String[] split = text.split(",");
+            String name = split[0];
+            int year = Integer.parseInt(split[1]);
+
+            if (name.length() > lname.length()){
+                lname = name;
+            }
+
+            sumYear += year;
+            count++;
+            
         }
+        double avgyear = (double) sumYear / count;
+        System.out.println("Longest name: " + lname);
+        System.out.println("Average of the birth years: " + avgyear);  
     }
 }   
