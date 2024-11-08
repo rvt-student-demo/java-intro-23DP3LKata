d1ced7a391173e6ac6b9d22bab8b1186b55b4429
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-11-08 06:55:35 +0000

    4/1 part: Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,16 +5,9 @@ public class App
     public static void main( String[] args )
     {
 
-        // Your first bank transfer
-        
-        Account acc1 = new Account("Matthews account", 1000.0);
-        Account acc2 = new Account("My account", 0.0);
+        Product product = new Product("Banana", 1.1, 13);
 
-        acc1.withdraw(100.0);
-        acc2.deposit(100.0);
-
-        System.out.println(acc1);
-        System.out.println(acc2);
+        product.printProduct();
 
     }
 }   
---
+++ src/main/java/lv/rvt/Product.java
@@ -0,0 +1,19 @@
+package lv.rvt;
+
+public class Product {
+
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Product (String initialName, double initialPrice, int initialQuantity) {
+        this.name = initialName;
+        this.price = initialPrice;
+        this.quantity = initialQuantity;
+    }
+
+    public void printProduct() {
+        System.out.println(this.name + ", price " + this.price + ", " + this.quantity + " pcs");
+    }
+
+}
