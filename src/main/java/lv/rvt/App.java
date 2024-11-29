6dc22cfcef819b8ae81ce0f91986eb81b544c50f
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2024-11-29 08:22:12 +0000

    Part 5/1: One Minute

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,24 +1,22 @@
 package lv.rvt;
 
-import java.util.*;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // Overloaded Counter (2 parts)
+        Timer timer = new Timer();
 
-        Counter counter = new Counter(5);
-        System.out.println(counter.value());
-        counter.increase();
-        System.out.println(counter.value());
-        counter.decrease();
-        System.out.println(counter.value());
-        counter.increase(5);
-        System.out.println(counter.value());
-        counter.decrease(6);
-        System.out.println(counter.value());
-        counter.decrease(10);
-        System.out.println(counter.value());
+        while (true) {
+            System.out.println(timer);
+            timer.advance();
+
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                System.out.println("An error occurred.");
+            }
+        }
     }
 }
