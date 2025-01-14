14b7c1b9f5d19857e87505c08e6ea9bf7c177d8c
23DP3LKata <166131602+23DP3LKata@users.noreply.github.com>
2025-01-14 10:11:26 +0000

    Chapter 50: Box

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,12 +7,14 @@ public class App
     public static void main ( String[] args )
 
     {
-        Box box = new  Box( 2.5, 5.0, 6.0 ) ;
+        Box box = new Box(1, 1, 1);
   
-        System.out.println( "Area: "  + box.area() + " volume: " + box. volume() );
-  
-        System.out.println( "topArea: "  + box.topArea() );
-  
+        System.out.println("height: " + box.height + " width: " + box.width + " length: " + box.length);
+        
+        System.out.println("Area: " + box.area() + " volume: " + box.volume());
+          
+        System.out.println("Small box: " + box.smallBox(box).volume());
+        System.out.println("Big box: " + box.biggerBox(box).volume());
     }
 }
   
--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -6,27 +6,57 @@ public class Box {
     public double height;
     public double length;
 
-    Box ( double width, double height, double length ) {
+
+    public Box(double width, double height, double length ) {
         this.width = width;
         this.height = height;
         this.length = length;
     }
 
-    double area() {
-        return 2 * faceArea() + 2 * topArea() + 2 *sideArea() ;
+    public Box(Box oldBox) {
+        this.width = oldBox.width;
+        this.height = oldBox.height;
+        this.length = oldBox.length;
     }
 
-    double volume() {
-        return width * height * length;
+
+
+    public double length() {
+        return this.length;
+    }
+    public double height() {
+        return this.height;
+    }
+    public double width() {
+        return this.width;
     }
 
-    double topArea() {
-        return width * length;
+
+
+    public Box biggerBox(Box oldBox) {
+        return new Box(1.25 * oldBox.width, 1.25 * oldBox.height, 1.25 * oldBox.length);
     }
-    double faceArea() {
-        return width * height;
+
+    public Box smallBox(Box oldBox) {
+        return new Box(0.75 * oldBox.width, 0.75 * oldBox.height, 0.75 * oldBox.length);
     }
-    double sideArea() {
-        return length * height;
+    
+
+    public double area() {
+        return 2 * faceArea() + 2 * topArea() + 2 * sideArea();
+    }
+
+    public double volume() {
+        return this.width * this.height * this.length;
+    }
+
+    private double topArea() {
+        return this.width * this.length;
+    }
+    private double faceArea() {
+        return this.width * this.height;
+    }
+    private double sideArea() {
+        return this.length * this.height;
     }
 }
